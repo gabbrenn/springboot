13cd4147baf5fe149e6dba9b7a273d58ef65202d
gabbrenn <gabvladimirbrenn@gmail.com>
2025-04-28 16:18:38 +0200

    fix: remove unused package:

--- src/main/java/rw/vladvisionlab/inzozi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/rw/vladvisionlab/inzozi/exceptions/GlobalExceptionHandler.java
@@ -2,7 +2,7 @@ package rw.vladvisionlab.inzozi.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+// import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
--- src/main/java/rw/vladvisionlab/inzozi/services/AuthenticationService.java
+++ src/main/java/rw/vladvisionlab/inzozi/services/AuthenticationService.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import rw.vladvisionlab.inzozi.dtos.AuthenticationRequest;
@@ -19,7 +19,7 @@ public class AuthenticationService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
+    // private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
     private final AppUserRepository appUserRepository;
 
