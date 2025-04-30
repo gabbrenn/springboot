88d031db13883dacb6eea9fba675e44a6d1d4580
gabbrenn <gabvladimirbrenn@gmail.com>
2025-04-30 10:24:20 +0200

    Fix Database connection

--- src/main/java/rw/vladvisionlab/inzozi/models/Products.java
+++ src/main/java/rw/vladvisionlab/inzozi/models/Products.java
@@ -1,12 +1,10 @@
 package rw.vladvisionlab.inzozi.models;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.*;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,20 +18,15 @@ public class Products {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
-
-    @Column(nullable = false)
     private String productName;
-
     private String description;
-
-    @Column(nullable = false)
     private int quantity;
+    @Column(name = "price", precision = 10, scale = 2)
+    private BigDecimal price;
 
-    @Column(nullable = false)
-    private float price;
 
     @CreationTimestamp
-    @Column(updatable = false)
+    @Column(columnDefinition = "DATETIME")
     private LocalDateTime createdAt;
     
 }
